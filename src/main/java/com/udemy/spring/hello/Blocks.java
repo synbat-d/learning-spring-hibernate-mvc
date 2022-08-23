83b944f5255f2147e4d9ed93fd606f7e385fcc2e
Symbat Dulatuly <synbat1999@gmail.com>
2022-08-24 01:00:35 +0600

    initial version of maven project without spring features

--- src/main/java/com/udemy/spring/hello/Blocks.java
+++
@@ -1,9 +0,0 @@
-package com.udemy.spring.hello;
-
-public class Blocks {
-    public static void main(String [] args) {
-        for (int i=0; i<5; i++) {
-            System.out.println("###");
-        }
-    }
-}
